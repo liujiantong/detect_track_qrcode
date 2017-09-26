bd35ee41853b2f4b52bcf27c55df5a0fe1d3b2dd
刘涛1 <tao.liu1@ikang.com>
2017-09-27 00:21:08 +0800

    refined structure

---
+++ scan_qr.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from sys import argv
+import zbar
+from PIL import Image
+
+
+if len(argv) < 2: exit(1)
+
+# create a reader
+scanner = zbar.ImageScanner()
+
+# configure the reader
+scanner.parse_config('enable')
+
+# obtain image data
+pil = Image.open(argv[1]).convert('L')
+width, height = pil.size
+raw = pil.tobytes()
+
+# wrap image data
+image = zbar.Image(width, height, 'Y800', raw)
+
+# scan the image for barcodes
+scanner.scan(image)
+
+# extract results
+for symbol in image:
+    # do something useful with results
+    print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
+
+# clean up
+del(image)
